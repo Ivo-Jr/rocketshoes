e541471f77c8ce4a2d1d0682bac722f833d7148d
IvoJr <maciel.jr95@gmail.com>
2021-04-19 05:42:35 -0300

    stock with SAGA

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,13 +4,9 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        const { product } = action;
 
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({ ...action.product, amount: 1 });
-        }
+        draft.push(product);
       });
 
     case '@cart/REMOVE':
@@ -22,11 +18,7 @@ export default function cart(state = [], action) {
         }
       });
 
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
-
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
 

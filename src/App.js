93820da4267e4b93129b7f99cbd8d068eebbbe23
IvoJr <maciel.jr95@gmail.com>
2021-04-20 18:16:39 -0300

    construction history

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import GlobalStyles from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+// import history from './services/history';
 import store from './store';
 
 function App() {
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -17,6 +17,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+// import history from '../../../services/history';
 import { formatPrice } from '../../../util/format';
 
 import { addToCartSuccess, updateAmountSuccess } from './actions';
@@ -49,6 +50,8 @@ function* addToCart({ id }) {
       priceFormatted: formatPrice(response.data.price),
     };
     yield put(addToCartSuccess(data));
+
+    // history.push('/cart');
   }
 }
 
